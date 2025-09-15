1817298dee794be0af4c0cb4e2054b32ac7ed16c
VladislavVenin <d4spda@yandex.ru>
2025-09-16 09:45:09 +1100

    Added console input

--- bot.py
+++ bot.py
@@ -2,14 +2,29 @@ import telegram
 import decouple
 import os
 import random
+import argparse
 import time
+
+
+def send_picture(chat_id, path, image=None):
+    if image is None:
+        images = os.listdir(path)
+        image = random.choice(images)
+    bot.send_document(
+        chat_id, document=open(f'{path}/{image}', 'rb')
+        )
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('image', type=str, default=None)
+args = parser.parse_args()
+image = args.image
 
 bot = telegram.Bot(token=decouple.config('BOT'))
 chat_id = decouple.config('CHANNEL')
 path = 'images'
-images = os.listdir(path)
+
 while True:
-    bot.send_document(
-        chat_id, document=open(f'{path}/{random.choice(images)}', 'rb')
-        )
-    time.sleep(decouple.config('REST'))
+    send_picture(chat_id, path, image)
+    image = None
+    time.sleep(int(decouple.config('REST')))
